a30ff0fb22b871c4445e98dca226ad2881ac1580
Fee1aRog <90037916+Fee1aRog@users.noreply.github.com>
2022-01-30 03:58:04 +0400

    1st lecture

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
+
 
 function App() {
+  function example(){
+  console.log('Image is clicked');
+}
+const colors= [
+  'orange',
+  'red',
+  'black'
+];
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" onClick={example()}/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,6 +27,15 @@ function App() {
         >
           Learn React
         </a>
+        <Button ragacaClass='Click' text='Click me'/>
+        <Button text='Button 2'/>
+        {
+          colors.map((color, index)=>{
+            return(
+              <span key={index} styl>{color}</span>
+            )
+          })
+        }
       </header>
     </div>
   );
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,8 @@
-export default function Button() {
+// export default function Button(props) 
+export default function Button({ragacaClass, text})
+{
     return (
-        <button>Click</button>
+        // <button className={"btn" + " " + props.ragacaClass} style={{backgroundcolor: 'yellow'}}> {props.text}</button>
+        <button className={`btn ${ragacaClass ? ragacaClass : ""}`}>{text}</button>
     )
 }
