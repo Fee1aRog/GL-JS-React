eeb92b4fff2d45ff2bbc93d329df3e1a9d7895b1
Fee1aRog <90037916+Fee1aRog@users.noreply.github.com>
2022-01-31 02:41:30 +0400

    2nd lecture

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,31 +2,60 @@ import { useState } from "react";
 
 export default function Form(){
 
-    const [state, setstate] = useState('test'); 
+    const [state, setstate] = useState('test');
+    
+    const [title, setTitle] = useState(true);
+
+    function changevalue(e) {
+         setstate(e.target.value);
+         console.log(state);
+    }
 
     function formSubmit(e) { 
         e.preventDefault();
         alert('Form is submitted!');
     }
 
-    // function FormClicked() {
-    //     alert('Form clicked');
-    // }
+    function FormClicked() {
+        // alert('Form clicked');
+        if (title) {
+            setTitle(false);
+        }
+        else {
+            setTitle(true);
+        }
+        
+    }
 
     const variable = true;
     
     return(
     <>
         {
-            variable ? <p>variable is True!</p> : <p>variable if False!</p> // null - თუ ფოლსის შემთხვევაში არაფერი არ უნდა გამოჩნდეს.
-        }
-        <form action='/Submitted_form' onSubmit={formSubmit}>
+            variable 
+            ?  
+            <form action='/Submitted_form' onSubmit={formSubmit}>
+                {
+                    title ?
+                    <h2>Form Title</h2>
+                    :null
+                }
             <textarea />
-            <br/>
-            <input value={state} onChange={(e) => setstate(e.target.value)} />
-            <br/>
-            <button type="button" onClick={() => alert('Form clicked')}> Submit </button>
+                <br/>
+                {/* {String(state)} */}
+                <p>{state}</p>
+                <br/>
+                    <input 
+                        value={state} 
+                        onChange={changevalue}
+                     />
+                <br/>
+            <button type="button" onClick={FormClicked}> Submit </button>
+            {/* <button type="button" onClick={() => alert('Form clicked')}> Submit </button> */}
         </form>
+            : <p>variable if False!</p> // null - თუ ფოლსის შემთხვევაში არაფერი არ უნდა გამოჩნდეს.
+        }
+        
     </>
     )
 }
